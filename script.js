193920905fcc46e9e85b1b13247a4cf5b41a6295
João <jpguerra1997@gmail.com>
2022-06-26 22:07:09 -0300

    entrega finalizada

---
+++ script.js
@@ -0,0 +1,21 @@
+function snapCrackle(maxValue) {
+    let text = "";
+    for (let value = 1; value <= maxValue; value++) {
+        if (value % 2 != 0 && value % 5 == 0) {
+            text += "SnapCrackle, ";
+            continue;
+        }
+        if (value % 2 !== 0) {
+            text += "Snap, ";
+            continue;
+        }
+        if (value % 5 == 0) {
+            text += "Crackle, ";
+            continue;
+        }
+        text += value + ", ";
+    }
+    console.log(text);
+}
+
+snapCrackle(15);
